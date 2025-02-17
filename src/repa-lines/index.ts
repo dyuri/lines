041ac24b012c3450e75342fd59c69dedad82a062
Gyuri Horak <dyuri@horak.hu>
2025-02-17 20:28:51 +0100

    webcomponent upload

--- src/repa-lines/index.ts
+++ src/repa-lines/index.ts
@@ -1,4 +1,4 @@
-import { copyImageToCanvasFromURL } from './imageUtils.ts';
+import { copyImageToCanvasFromURL, handleUploadImage } from './imageUtils.ts';
 import { setupSimpleLineDrawer } from './simpleLineDrawer.ts';
 import styles from './style.css?inline';
 
@@ -9,6 +9,9 @@ export default class RepaLines extends HTMLElement {
 
   #refCanvas?: HTMLCanvasElement;
   #lineCanvas?: HTMLCanvasElement;
+  #uploadBtn?: HTMLButtonElement;
+  #imageInput?: HTMLInputElement;
+  #resetBtn?: HTMLButtonElement;
   src: string = '';
 
   constructor() {
@@ -22,6 +25,11 @@ export default class RepaLines extends HTMLElement {
         ${styles}
       </style>
       <div class="imagecontainer">
+        <div class="toolbar">
+          <button id="uploadBtn">Upload</button>
+          <button id="resetBtn">Reset</button>
+          <input type="file" id="fileinput" accept="image/*" />
+        </div>
         <canvas class="refcanvas" id="refcanvas"></canvas>
         <canvas class="linecanvas" id="linecanvas"></canvas>
       </div>
@@ -30,8 +38,21 @@ export default class RepaLines extends HTMLElement {
 
     this.#refCanvas = shadow.querySelector<HTMLCanvasElement>('#refcanvas')!;
     this.#lineCanvas = shadow.querySelector<HTMLCanvasElement>('#linecanvas')!;
+    this.#uploadBtn = shadow.querySelector<HTMLButtonElement>('#uploadBtn')!;
+    this.#resetBtn = shadow.querySelector<HTMLButtonElement>('#resetBtn')!;
+    this.#imageInput = shadow.querySelector<HTMLInputElement>('#fileinput')!;
 
+    this.#resetBtn.addEventListener('click', () => {
+      this.reset();
+    });
+
+    // TODO implement as method, so it would be easier to start/stop
     setupSimpleLineDrawer(this.#lineCanvas, this.#refCanvas);
+
+    handleUploadImage(this.#refCanvas, this.#imageInput);
+    this.#uploadBtn.addEventListener('click', () => {
+      this.#imageInput?.click();
+    });
 
     if (this.src) {
       this.loadSrc();
@@ -42,19 +63,23 @@ export default class RepaLines extends HTMLElement {
     console.log('attributeChangedCallback', name, newValue);
     if (name === 'src') {
       this.src = newValue;
-      if (this.#refCanvas && this.#lineCanvas) {
-        this.#refCanvas.width = 0;
-        this.#refCanvas.height = 0;
-        this.#lineCanvas.width = 0;
-        this.#lineCanvas.height = 0;
-        this.loadSrc();
-      }
+      this.reset();
     }
   }
 
   loadSrc() {
     if (this.#refCanvas && this.#lineCanvas) {
       copyImageToCanvasFromURL(this.#refCanvas, this.src);
+    }
+  }
+
+  reset() {
+    if (this.#refCanvas && this.#lineCanvas) {
+      this.#refCanvas.width = 0;
+      this.#refCanvas.height = 0;
+      this.#lineCanvas.width = 0;
+      this.#lineCanvas.height = 0;
+      this.loadSrc(); // TODO
     }
   }
 }
